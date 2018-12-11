67ad553edf2ccbe9de8828f30176c11ed526e27d
AndrewYW <andrewwang39@gmail.com>
2018-12-11 09:50:04 -0500

    move users.rb, better directory structure

---
+++ lib/users.rb
@@ -0,0 +1,28 @@
+require 'sqlite3'
+require 'singleton'
+
+class UserDBConnection < SQLite3::Database
+    include Singleton
+
+    def initialize('users.db')
+        self.type_translation = true
+        self.results_as_hash = true
+    end
+end
+
+class User
+
+    def self.all
+        data = UserDBConnection.instance.execute("SELECT * FROM users")
+        data.map{ |datum| User.new(datum)}
+    end
+
+    def initialize(options)
+        
+    end
+
+    def create
+    end
+
+    def update
+    end
