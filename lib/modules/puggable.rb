5dfdcdc17a9798c680ae4a5d3c0a72edc46e7d44
AndrewYW <andrewwang39@gmail.com>
2019-06-27 12:34:14 -0400

    add user counts, nameable pug teams

--- lib/modules/puggable.rb
+++ lib/modules/puggable.rb
@@ -6,16 +6,18 @@ module Puggable
     category = event.server.create_channel("PUGS", 4)
     category.position = 1
 
-    event.server.create_channel("pug-lobby", 2, { parent: category })
-    event.server.create_channel("TEAM-1", 2, { parent: category })
-    event.server.create_channel("TEAM-2", 2, { parent: category })
+    first = event.content.split(" ")[1] || 'TEAM-1'
+    second = event.content.split(" ")[2] || 'TEAM-2'
+
+    event.server.create_channel("pug-lobby", 2, { parent: category, user_limit: 99 })
+    event.server.create_channel(first, 2, { parent: category, user_limit: 12})
+    event.server.create_channel(second, 2, { parent: category, user_limit: 12})
 
     nil
   end
 
   def end_pugs(event)
     pug_role = event.server.roles.find {|role| role.id == CFG['pug']}
-    event << pug_role.mention
     event << event.user.mention + " HAS DECREED PUGS ARE OVER"
 
     category = event.server.channels.find {|channel| channel.name == "PUGS"}
